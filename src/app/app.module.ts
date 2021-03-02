569cd67499a752b89bca5b185230f10902eb036c
Raul Monroy <sBlinkRst@outlook.com>
2021-03-02 11:08:22 -0600

    se agrego Http peticiones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
 import {GifsModule} from './gifs/gifs.module';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {GifsModule} from './gifs/gifs.module';
   imports: [
     BrowserModule,
     SharedModule,
+    HttpClientModule,
     GifsModule,
     AppRoutingModule
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,25 +1,36 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {GifsInterface} from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
   private historial: string[] = [];
+  private apiKey = 'RAfjRiIuzXB1B1aUcGytKXP5qrIoxmoP';
+  private url = 'http:///api.giphy.com/v1/gifs/search?';
+  private urlApiKey = 'api_key=';
+  private urlQuery = '&q=';
+  private urlEnd = '&limit=10&lang=es';
 
   get Historial(): string[]{
     return [...this.historial];
   }
+  constructor(private http: HttpClient) {
+  }
 
   BuscarGifs(valor: string): void{
     valor = valor.trim().toLocaleLowerCase();
-
+    const url = `${this.url}${this.urlApiKey}${this.apiKey}${this.urlQuery}${valor}${this.urlEnd}`;
     if (!this.historial.includes(valor))
     {
       this.historial.unshift(valor);
       this.historial = this.historial.splice(0, 10);
     }
-    console.log(this.historial);
+    this.http.get(url)
+      .subscribe((resp: GifsInterface) => {
+        console.log(resp.data);
+      });
   }
 
-  constructor() { }
 }
