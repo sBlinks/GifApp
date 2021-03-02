c9ddfe88fdc41e9382c9baddafb636f4d013c55f
Raul Monroy <sBlinkRst@outlook.com>
2021-03-02 09:49:21 -0600

    viewchild en input

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class BusquedaComponent implements OnInit {
-
+  @ViewChild('Txtbuscar') txtBuscar: ElementRef<HTMLInputElement>;
   constructor() { }
-
   ngOnInit(): void {
   }
 
+  Buscar(): string {
+    const txtbuscar = this.txtBuscar.nativeElement;
+    const valor = txtbuscar.value;
+    txtbuscar.value = '';
+    return valor;
+  }
 }
